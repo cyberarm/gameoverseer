da0741a14594a9f2bf48dbe8a66b81fc2dcb9851
cyberarm <matthewlikesrobots@gmail.com>
2013-05-18 15:06:23 -0500

    Don't call private method, add default service version.

--- lib/gameoverseer/service/service.rb
+++ lib/gameoverseer/service/service.rb
@@ -2,7 +2,11 @@ class GameOverseer
   class Service
     def self.inherited(subclass)
       $log.info "Service #{subclass} activated." if $log
-      GameOverseer::ServiceManager::SERVICES.push(subclass.new.initialize)
+      GameOverseer::ServiceManager::SERVICES.push(subclass.new)
+    end
+
+    def version
+      "unknown"
     end
   end
 end
