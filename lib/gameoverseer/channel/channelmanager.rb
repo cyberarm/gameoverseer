6af26e776d064c9d82a8e9d963ee27373dbfb9b0
cyberarm <matthewlikesrobots@gmail.com>
2013-07-20 21:39:59 -0500

    Fixed #2, channels now talk to services

--- lib/gameoverseer/channel/channelmanager.rb
+++ lib/gameoverseer/channel/channelmanager.rb
@@ -4,15 +4,17 @@ class GameOverseer
   class ChannelManager
     CHANNELS = []
 
+    # Subscribe "channel" to ServiceClass
     def self.subscribe(channel, klass)
       CHANNELS << {channel: channel.to_s, klass: klass}
     end
 
+    # Push data to subscribers of "requested_channel"
     def push(requested_channel, data)
       CHANNELS.each do |channel|
-        if requested_channel == channel
+        if requested_channel == channel[:channel]
           GameOverseer::ServiceManager::SERVICES.each do |service|
-            if service.name == channel[:klass].class.name
+            if service.class.name == channel[:klass].class.name
               service.send(:update, data)
             end
           end
